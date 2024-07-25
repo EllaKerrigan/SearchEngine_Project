bb856f44a2ebe2bd75af0cbd6b9ddb159ed63885
EllaKerrigan <ella.k.kerrigan@gmail.com>
2024-07-25 12:51:15 -0700

    Crawled json file data and crawl.py updated

--- crawl.py
+++ crawl.py
@@ -1,19 +1,11 @@
-# allows us to interact with reddit API's
 import praw
-# time related funcs 
 import time
-import sys 
-# handles json files - important here
+import sys
 import json
-# read/write 
 import os
-#handles error requests 
 from prawcore.exceptions import RequestException, TooManyRequests
-# makes HTTP requests simpler
-import requests 
-# parse HTMP AND XML docs 
+import requests
 from bs4 import BeautifulSoup
-#runs tasks in parallel 
 from multiprocessing import Pool, Manager
 from pathlib import Path
 
@@ -57,20 +49,27 @@ subredditNames = ['Love Island', 'Gilmore Girls', 'Sephora', 'Tiktok', 'Instagra
 targetSizeOfData = 100 * 1024 * 1024 
 
 #Tracking crawled IDs - dont want to re-crawl
-file_path = Path('cralwed_ids.json')
+#file_path = Path('cralwed_ids.json')
 # if crawled_ids.json exists. It tries to load the data
 # if there's an error, it uses an empty set. 
 # If the file doesn't exist, it creates the file with empty list
-if file_path.exists():
-    try:
-        with open(file_path, 'r') as f:
-            crawled_ids = set(json.load(f))
-    except json.JSONDecodeError:
-        crawled_ids = set()
-else:
+# if file_path.exists():
+#     try:
+#         with open(file_path, 'r') as f:
+#             crawled_ids = set(json.load(f))
+#     except json.JSONDecodeError:
+#         crawled_ids = set()
+# else:
+#     crawled_ids = set()
+#     with open(file_path, 'w') as f:
+#         json.dump(list(crawled_ids),f)
+try:
+    with open('crawled_ids.json', 'r') as f:
+        crawled_ids = set(json.load(f))
+except (FileNotFoundError, json.JSONDecodeError):
     crawled_ids = set()
-    with open(file_path, 'w') as f:
-        json.dump(list(crawled_ids),f)
+    with open('crawled_ids.json', 'w') as f:
+        json.dump(list(crawled_ids), f)
         
 #Type of posts to crawl
 postType = ['top', 'new', 'controversial', 'trending', 'hot']
@@ -138,10 +137,11 @@ def crawl_subreddit(args):
                     json.dump(subredditData, f, indent = 5)
                     #prints file name and size
                     file_size_mb = os.path.getsize(fileName)/(1024*1024)
+                    print(f"{fileName}{file_size_mb:.2f}MB")
                 
                 #handles rate limit 
             except TooManyRequests:
-                print("Hit rate limit, sleeping for 60 seonds")
+                print("Hit rate limit, sleeping for 60 seconds")
                 time.sleep(60)
         crawled_ids = list(crawled_idsSet)
         
